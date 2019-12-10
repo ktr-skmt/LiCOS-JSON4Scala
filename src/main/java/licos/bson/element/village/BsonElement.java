4a4c19236532b73564bc53165ce3acccfe58ce31
Kotaro Sakamoto <sakamoto-kotaro-pn@ynu.jp>
2019-12-09 20:22:38 -0800

    v 0.2.0 (removed morphia)

--- src/main/java/licos/bson/element/village/BsonElement.java
+++
@@ -1,9 +0,0 @@
-package licos.bson.element.village;
-
-import licos.json.element.village.JsonElement;
-
-public abstract class BsonElement {
-    public JsonElement toJson() {
-        return null;
-    }
-}
---
+++ src/main/scala/licos/json/validation/village/ArchitectureValidation.scala
@@ -0,0 +1,3 @@
+package licos.json.validation.village object ArchitectureValidation {
+
+}
