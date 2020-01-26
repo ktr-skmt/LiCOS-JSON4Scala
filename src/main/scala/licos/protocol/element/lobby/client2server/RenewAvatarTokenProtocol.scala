c69e6e9df4f979c85c8f6b09ed524dec0339f031
Kotaro Sakamoto <sakamoto-kotaro-pn@ynu.jp>
2020-01-26 02:04:44 -0800

    support for protocol changes

--- src/main/scala/licos/protocol/element/lobby/client2server/RenewAvatarTokenProtocol.scala
+++ src/main/scala/licos/protocol/element/lobby/client2server/RenewAvatarTokenProtocol.scala
@@ -3,11 +3,12 @@ package licos.protocol.element.lobby.client2server
 import java.util.UUID
 
 import licos.json.element.lobby.client2server.JsonRenewAvatarToken
+import licos.knowledge.{Data2Knowledge, Lobby}
 import play.api.libs.json.{JsValue, Json}
 
-final case class RenewAvatarTokenProtocol(token: UUID) extends Client2ServerLobbyMessageProtocol {
+final case class RenewAvatarTokenProtocol(token: UUID, lobby: Lobby) extends Client2ServerLobbyMessageProtocol {
 
-  private lazy val json: Option[JsonRenewAvatarToken] = Some(new JsonRenewAvatarToken(token.toString))
+  private lazy val json: Option[JsonRenewAvatarToken] = Some(new JsonRenewAvatarToken(token.toString, lobby.label))
 
   override def toJsonOpt: Option[JsValue] = json.map { j =>
     Json.toJson(j)
@@ -16,12 +17,13 @@ final case class RenewAvatarTokenProtocol(token: UUID) extends Client2ServerLobb
 
 object RenewAvatarTokenProtocol {
 
-  def read(jsonRenewAvatarToken: JsonRenewAvatarToken): Option[RenewAvatarTokenProtocol] = {
-    Some(
+  def read(json: JsonRenewAvatarToken): Option[RenewAvatarTokenProtocol] = {
+    Data2Knowledge.lobbyOpt(json.lobby).map { lobby: Lobby =>
       RenewAvatarTokenProtocol(
-        UUID.fromString(jsonRenewAvatarToken.token)
+        UUID.fromString(json.token),
+        lobby
       )
-    )
+    }
   }
 
 }
